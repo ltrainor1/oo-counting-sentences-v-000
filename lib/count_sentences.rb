b9dbd9997454e722253aa4655ebe69e8c32bd661
Lindsey Trainor <ltrainor38@gmail.com>
2018-01-13 01:04:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    period = self.end_with?(".")
+    if self.question? || self.exclamation? || period
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?") 
+      return true
+    else 
+      return false  
+    end 
+    
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def count_sentences
-
+    sentences = self.split(/[\.\?\!]\s[a-zA-Z]/)
+    sentences.length 
   end
 end
